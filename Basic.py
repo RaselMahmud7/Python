a80ae975aeca5daf2642528abaa7d68419576453
RaselMahmud7 <r.mahmud2207@gmail.comud2207@gmail.com>
2025-03-13 23:49:49 +0100

    Basic Mean

---
+++ Basic.py
@@ -0,0 +1,8 @@
+# Description: This file contains the basic python code for the beginners.
+FirstMonth = 23
+SecondMonth = 32
+ThirdMonth = 64
+# Calculating the mean of the three months
+mean = (FirstMonth+SecondMonth+ThirdMonth ) /3
+print(mean)
+
