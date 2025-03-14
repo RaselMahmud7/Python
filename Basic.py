c9912b093062ee5b6aac814f959f1ce34b60970e
RaselMahmud7 <r.mahmud2207@gmail.comud2207@gmail.com>
2025-03-14 01:15:14 +0100

    Calculation Added

--- Basic.py
+++ Basic.py
@@ -7,8 +7,16 @@ mean = (FirstMonth+SecondMonth+ThirdMonth ) /3
 print(mean)
 
 """
-#Task 1: Write a Python program to take the input of two numbers and print the sum of the two numbers.
+"""#Task 1: Write a Python program to take the input of two numbers and print the sum of the two numbers.
 
 Name = input("Enter your name: ")
 #print("Hello", Name)
-print("Welcome to the Python World."  Name  "!")
+print("Welcome to the Python World,"+  Name  ,"!")"""
+
+
+TilesNeed = 9*7 + 5*7
+print("Total Tiles Need: ", TilesNeed)
+# Fill this in with an expression that calculates how many tiles will be left over.
+TilesBuy = 17*6
+leftOver = TilesBuy - TilesNeed
+print("Numeber of tiles left over: ", leftOver)
